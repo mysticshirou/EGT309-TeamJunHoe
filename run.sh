fe61db06b8362da898d5455874d75df6b09ad094
Harish Kanna <mysticemiya@gmail.com>
2025-11-26 23:31:06 +0800

    Containerized kedro run with Dockerfile, edited run.sh to run the Docker container

--- run.sh
+++ run.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
+IMAGE_NAME=kedro-app
 
-ENV_NAME="kedro_project"
+docker build -t $IMAGE_NAME -f Dockerfile-kedro .
 
-# Check if environment already exists
-if conda info --envs | grep -q "$ENV_NAME"; then
-    echo "Conda environment '$ENV_NAME' already exists. Skipping creation."
-else
-    echo "Creating Conda environment '$ENV_NAME'..."
-    conda create -y -n "$ENV_NAME" python=3.11
-fi
-
-# Activate the environment
-echo "Activating Conda environment '$ENV_NAME'..."
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate "$ENV_NAME"
-
-# Install dependencies
-if [ -f requirements.txt ]; then
-    echo "Installing dependencies from requirements.txt..."
-    pip install --upgrade pip
-    pip install -r requirements.txt
-else
-    echo "requirements.txt not found. Skipping dependency installation."
-fi
-
-# Run Kedro
-echo "Running Kedro pipeline..."
-kedro run
+# Mount data so that local data folder is changed
+docker run --rm \
+    -v "$(pwd)/data:/app/data" \
+    $IMAGE_NAME
