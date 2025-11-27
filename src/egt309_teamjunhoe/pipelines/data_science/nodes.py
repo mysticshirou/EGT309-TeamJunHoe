c48c5c673bde658fa6f77f61d75b63febae93f09
Darren Foo <originaluuser@gmail.com>
2025-11-27 13:51:06 +0800

    Added parameters and initial nodes for kedro model pipeline

--- src/egt309_teamjunhoe/pipelines/data_science/nodes.py
+++ src/egt309_teamjunhoe/pipelines/data_science/nodes.py
@@ -1,2 +1,8 @@
 from interfaces import Model
 
+def model_choice(params):
+    if params.get("decision_tree_model", None) == True:
+        class ChosenModel(Model):
+            
+
+    return ChosenModel
