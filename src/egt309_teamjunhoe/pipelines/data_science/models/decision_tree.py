35f623a320710c0cbe4f773da133d6161c7f4c2d
Darren Foo <originaluuser@gmail.com>
2025-11-27 14:43:52 +0800

    Created decision tree model

---
+++ src/egt309_teamjunhoe/pipelines/data_science/models/decision_tree.py
@@ -0,0 +1,16 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from interfaces import Model
+from typing import Any
+
+class DecisionTree(Model):
+    @staticmethod
+    def train(X_train, y_train, params: dict[Any, Any]) -> Any:
+        tree = DecisionTreeClassifier(**params.get("decision_tree_settings", {}))
+        trained_model = tree.fit(X_train, y_train)
+        return trained_model
+
+    @staticmethod
+    def eval(model, X_test, y_test, params: dict[Any, Any]) -> Any:
+        y_pred = model.predict(X_test)
+        return accuracy_score(y_test, y_pred)
