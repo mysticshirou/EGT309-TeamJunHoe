f3d7a9dce56426b4b315da6972d9d0445912f109
Darren Foo <originaluuser@gmail.com>
2025-11-27 14:11:58 +0800

    Added model choice function to data_science kedro nodes

--- src/egt309_teamjunhoe/pipelines/data_science/nodes.py
+++ src/egt309_teamjunhoe/pipelines/data_science/nodes.py
@@ -1,8 +1,21 @@
 from interfaces import Model
+from typing import Any
+from sklearn.tree import DecisionTreeClassifier
+from models import adaboost
 
-def model_choice(params):
-    if params.get("decision_tree_model", None) == True:
-        class ChosenModel(Model):
-            
+# --------------------------------------------------------------------------------------------------------------------------------------------------------
+#   Model Nodes
+# --------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    return ChosenModel
+def model_choice(params) -> Model:
+    match params.get("model_choice", None):
+        case "decision_tree":
+            model = adaboost.AdaBoost()
+        case "adaboost":
+            model = decision_tree.DecisionTree()
+        case _:
+            raise ValueError(f"\"{params.get('model_choice'), None}\" is not a valid model choice")
+
+    return model
+
+# def model_train(model: Model, X_train, y_train, params):
