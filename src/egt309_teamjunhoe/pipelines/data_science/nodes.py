68a8c85c4b74321113189dfbbe6219edd0861ff0
Darren Foo <originaluuser@gmail.com>
2025-11-27 14:16:43 +0800

    Added training and eval wrappers for model

--- src/egt309_teamjunhoe/pipelines/data_science/nodes.py
+++ src/egt309_teamjunhoe/pipelines/data_science/nodes.py
@@ -18,4 +18,10 @@ def model_choice(params) -> Model:
 
     return model
 
-# def model_train(model: Model, X_train, y_train, params):
+def model_train(model: Model, X_train, y_train, params: dict[Any, Any]):
+    trained_model = model.train(X_train, y_train, params)
+    return trained_model
+
+def model_eval(trained_model: Model, X_test, y_test, params: dict[Any, Any]):
+    score = trained_model.eval(trained_model, X_test, y_test, params)
+    return score
