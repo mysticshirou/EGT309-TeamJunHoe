766e6070c8be811db0e2e67890f9d4198e9c777d
Darren Foo <originaluuser@gmail.com>
2025-12-02 17:34:08 +0800

    Fixed knn not being able to save to json

--- src/egt309_teamjunhoe/pipelines/data_science/models/knn.py
+++ src/egt309_teamjunhoe/pipelines/data_science/models/knn.py
@@ -1,8 +1,8 @@
 from .interfaces import Model
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import make_scorer, fbeta_score
 from sklearn.neighbors import KNeighborsClassifier
 from skopt import BayesSearchCV
-from .model_utils import read_bs_search_space
+from .model_utils import read_bs_search_space, generate_report
 from .registry import register_model
 
 import matplotlib.pyplot as plt
@@ -15,32 +15,32 @@ class KNN(Model):
         search_space = read_bs_search_space(params.get("knn_bayes_search_search_space", {}))
         assert len(search_space) > 0
 
+        # Create scorer for model
+        fbeta_scorer = make_scorer(fbeta_score, beta=params.get("beta"))
+
         if params.get("knn_auto_optimize") == True:
             knn = KNeighborsClassifier()
             model = BayesSearchCV(
                 knn,
                 search_space,
+                scoring=fbeta_scorer,
                 random_state=params.get("random_state"),
                 **params.get("knn_bayes_search_settings", {})
             )
+            trained_model = model.fit(X_train, y_train).best_estimator_
         else:
             model = KNeighborsClassifier(**params.get("knn_settings", dict()))
-        model.fit(X_train, y_train)
-        return model, model.get_params()
+            trained_model = model.fit(X_train, y_train)
+
+        return trained_model, trained_model.get_params()
     
     @staticmethod
     def eval(model, X_test, y_test, params):
+        # Probabilities for positive class
+        y_prob = model.predict_proba(X_test)[:, 1]
+        # Predict classes
         y_pred = model.predict(X_test)
-        report = classification_report(y_test, y_pred, output_dict=True)
 
-        # Creating classification report as matplotlib plot
-        cf_matrix = confusion_matrix(y_test, y_pred)
-        fig, ax = plt.subplots(figsize=(8, 6))
-        labels = ["False", "True"]
-        sns.heatmap(cf_matrix, annot=True, fmt="d", ax=ax)
-        ax.set_xticklabels(labels)
-        ax.set_yticklabels(labels)
-        ax.set_ylabel("Actual")
-        ax.set_xlabel("Predicted")
+        report, fig = generate_report(y_test, y_prob, y_pred, params.get("beta"))
 
         return report, fig
