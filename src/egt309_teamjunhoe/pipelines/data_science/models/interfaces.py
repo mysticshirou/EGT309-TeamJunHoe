c1332d5e78c53205c4fb076456f6657df799997d
Harish Kanna <mysticemiya@gmail.com>
2025-12-06 00:54:23 +0800

    Added comments and docstrings to Model interface and Model Registry

--- src/egt309_teamjunhoe/pipelines/data_science/models/interfaces.py
+++ src/egt309_teamjunhoe/pipelines/data_science/models/interfaces.py
@@ -1,9 +1,35 @@
 from typing import Protocol, Any
 import pandas as pd
+from matplotlib.figure import Figure
 
 class Model(Protocol):
+    # train method should return the model and the dictionary of all model parameters
     @staticmethod
-    def train(X_train, y_train, params: dict[Any, Any]) -> Any: ...
+    def train(X_train, y_train, params: dict[Any, Any]) -> tuple[Any, dict]:
+        """
+        Docstring for train
+        
+        :param X_train: Feature columns used for training
+        :param y_train: Target column used for training
+        :param params: Used by Kedro node to automatically pass in parameters
+        :type params: dict[Any, Any]
+        :return: trained model and parameters used by the model
+        :rtype: tuple[Any, dict[Any, Any]]
+        """
+        ...
     
+    # eval methods should evaluate the trained model and return the metrics report and plot
     @staticmethod
-    def eval(model, X_test, y_test, params: dict[Any, Any]) -> Any: ...
+    def eval(model, X_test, y_test, params: dict[Any, Any]) -> tuple[dict, Figure]: 
+        """
+        Docstring for eval
+        
+        :param model: Trained model created by train method
+        :param X_test: Feature columns used for testing
+        :param y_test: Target column used for testing
+        :param params: Used by Kedro node to automatically pass in parameters
+        :type params: dict[Any, Any]
+        :return: metrics report and metrics plot
+        :rtype: tuple[dict[Any, Any], Figure]
+        """
+        ...
--- src/egt309_teamjunhoe/pipelines/data_science/models/registry.py
+++ src/egt309_teamjunhoe/pipelines/data_science/models/registry.py
@@ -1,7 +1,13 @@
-MODEL_REGISTRY = {}
+# Initializes an empty dictionary to store key-value pairs of names and classes 
+MODEL_REGISTRY: dict[str, type] = {}
 
+# Function that accepts a name as an argument (becomes the key)
 def register_model(name: str):
+    # Returns a function that accepts a class as an argument (becomes the value)
     def decorator(cls):
+        # Adds to dictionary
         MODEL_REGISTRY[name] = cls
+        # Returns the class so when this is used as a decorator around the class
+        # the class will still work fine
         return cls
     return decorator
